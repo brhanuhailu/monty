41268448405def591d50082c7b751d4c1b4afadc
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-10-08 11:53:02 -0700

    Update monty.h

--- monty.h
+++ monty.h
@@ -1,15 +1,17 @@
-#ifndef MONTY_H
-#define MONTY_H
+#ifndef MONTY_H_
+#define MONTY_H_
+
+#define UN __attribute__((__unused__))
+
+/* Standard Libraries */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <ctype.h>
 
-#define DELIMITERS " \t\n\r"
-#define STACK 1
-#define QUEUE 0
+#include <string.h>
+
+/* Structs and Lists */
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -22,9 +24,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -37,53 +39,64 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
 /**
- * struct gbl - some useful things.
- * @namefile: name of the file to read.
- * @nimbers: numbers to add to the stack.
- * @buffer: the line read by getline.
- * @com: opcode selected.
- * Description: some useful things.
+ * struct list_s - linked list of struct instruction
+ * @n: number of line
+ * @inst: instruction of opcode and its function
+ * @next: points to the next element of the linked list
+ * Description: Single linked list of the struct instruction
  */
-typedef struct glb
+typedef struct list_s
 {
-        FILE *namefile;
-        char *numbers;
-        char *buffer;
-        char *com;
-        int mode;
-}tool;
+	int n;
+	char *inst;
+	struct list_s *next;
+} list_t;
 
-int get_int_at_index(stack_t *head, unsigned int index);
-void delete_nodeint_at_index(stack_t **head, unsigned int index);
-void add_dnodeint_end(stack_t **head, const int n);
-void op_func(int times, char *option, stack_t **h);
-void add_dnodeint(stack_t **head, const int n);
-void _queue(stack_t **h, unsigned int times);
-void _stack(stack_t **h, unsigned int times);
-void _pchar(stack_t **h, unsigned int times);
-void _split(char *buffer, char *delimiters);
-void _pstr(stack_t **h, unsigned int times);
-void _push(stack_t **h, unsigned int times);
-void _pall(stack_t **h, unsigned int times);
-void _pint(stack_t **h, unsigned int times);
-void _swap(stack_t **h, unsigned int times);
-void _rotl(stack_t **h, unsigned int times);
-void _rotr(stack_t **h, unsigned int times);
-void _pop(stack_t **h, unsigned int times);
-void _add(stack_t **h, unsigned int times);
-void _nop(stack_t **h, unsigned int times);
-void _sub(stack_t **h, unsigned int times);
-void _div(stack_t **h, unsigned int times);
-void _mul(stack_t **h, unsigned int times);
-void _mod(stack_t **h, unsigned int times);
-int stack_len(stack_t *h);
-int _isdigit(char *str);
-void _free(stack_t *h);
+extern list_t *list_opcode;
 
-extern tool tools;
+/* Prototype Functions */
 
-#endif /*  MONTY_H */
+int get_file(char *str);
+void check_file(char *str);
+list_t *create_instruction(list_t **head, char *str, int n, FILE *fp);
+void (*ptr_opcode(list_t *head))(stack_t **stack, unsigned int line_number);
+char *_strdup(char *str, FILE *fp);
+int _strlen(char *str);
+int clean_string(char *buff, char *str);
+void free_list_opcode(list_t *head);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void nothing(stack_t **stack, unsigned int line_number);
+int cmp_inst(char *str1, char *str);
+stack_t *add_nodeint(stack_t **head, const int n);
+void free_all(list_t *list_opcode, stack_t *stack);
+void free_list_stack(stack_t *head);
+void swap(stack_t **stack, UN unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+int stack_len(stack_t *head);
+void pop(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(UN stack_t **stack, UN unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+int check_arg(char *buff);
+int cpy_arg(list_t *t, char *buff);
+void print_error_push(stack_t **stack, int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
+stack_t *add_dnodeint_end(stack_t **head, const int n);
+void core_stack_queue(stack_t **stack, unsigned int line_number, int sq);
+void queue(stack_t **stack, unsigned int line_number);
+void stack(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, UN unsigned int line_number);
+void rotl(stack_t **stack, UN unsigned int line_number);
+void rotr(stack_t **stack, UN unsigned int line_number);
+
+#endif /* MONTY_H_ */
